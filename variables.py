cc8476466cc146a73cd5976f395fd85a21fb3548
jfranciscoyr <yuman3872@gmail.com>
2023-12-11 17:44:41 -0600

    Subir proyecto inicialmente

---
+++ variables.py
@@ -0,0 +1,19 @@
+from clases import Rectangulo
+
+
+variable="hola mundo 2"
+print(variable)
+
+
+x=1
+y=1
+
+if x<y:
+    print(x,"menor a y")
+elif x>y:
+    print(y,"es mayor a X")
+else:
+    print("son iguales")
+
+   
+rectangulo = Rectangulo(5,8)
